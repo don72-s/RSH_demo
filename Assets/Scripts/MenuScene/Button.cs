1546b1d5c1a0f1227f9965d29de65025958bdde8
김민태 <66211881+don72-s@users.noreply.github.com>
2024-03-24 19:09:32 +0900

    swipe 메뉴 보정
    크기 변화 추가

---
+++ Assets/Scripts/MenuScene/Button.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+
+    private int myIdx;
+    private float unitDistance;
+
+    private float smallSize = 0.8f;
+
+    public void InitData(int _idx, float _unitDistance) { 
+        myIdx = _idx;
+        unitDistance = _unitDistance;
+    }
+
+    /// <summary>
+    /// 0~1 사이의 스크롤바 값이 들어옴
+    /// </summary>
+    /// <param name="_barValue"></param>
+    public void SetButtonSize(float _barValue) { 
+        
+        float localUnitDistance = Mathf.Abs(Mathf.Clamp(_barValue - (myIdx * 2 * unitDistance), -unitDistance * 2, unitDistance * 2));
+
+        Debug.Log(myIdx + " idx = " + localUnitDistance);
+
+        float localValue = 1 - ((localUnitDistance / (unitDistance * 2)) * (1 - smallSize));
+
+        transform.localScale = new Vector2(localValue, localValue);
+
+    }
+
+}
---
+++ Assets/Scripts/MenuScene/SwipeScript.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwipeScript : MonoBehaviour
+{
+
+    delegate void FocusSizeDelegate(float _scrollValue);
+
+
+
+    [SerializeField]
+    Scrollbar scrollBar;
+
+    [SerializeField]
+    GameObject contentParent;
+
+
+    FocusSizeDelegate focusSizeDelegate = null;
+
+    float curPos = 0;
+    float destPos = 0;
+
+    List<Button> buttonL;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Recalculate();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+
+            Recalculate();
+
+        }
+
+
+        if (Input.GetMouseButton(0))
+        {
+            curPos = scrollBar.value;
+
+        } else if (Input.GetMouseButtonUp(0)) {
+
+            if (curPos < 0)
+            {
+                destPos = 0;
+            }
+            else if (curPos > 1)
+            {
+                destPos = 1;
+            }
+            else
+            {
+                int destIdx = ((int)(curPos / unitDistance));
+                destIdx = destIdx % 2 == 0 ? destIdx : destIdx + 1;
+                destPos = destIdx * unitDistance;
+            }
+
+        }else {
+
+            scrollBar.value = Mathf.Lerp(scrollBar.value, destPos, 0.1f);
+
+        }
+
+        focusSizeDelegate(Mathf.Clamp(scrollBar.value, 0, 1));
+
+    }
+
+
+    float unitDistance = 0.5f;
+
+
+    public void Recalculate() {
+
+        focusSizeDelegate = null;
+
+        if (contentParent.transform.childCount <= 1) {
+
+            //todo : 예외 처리
+            return;
+        }
+
+        buttonL = new List<Button>();
+
+        unitDistance = 1f / ((contentParent.transform.childCount - 1) * 2);
+
+        for (int i = 0; i < contentParent.transform.childCount; i++) {
+            buttonL.Add(contentParent.transform.GetChild(i).GetComponent<Button>());
+            buttonL[i].InitData(i, unitDistance);
+            focusSizeDelegate += buttonL[i].SetButtonSize;
+        }
+
+    }
+
+
+}
